a4c7e1d04cda82d3f2b9d62b73c569e303db7b0f
Gleb Winterson <gtwototoo@gmail.com>
2024-01-10 15:00:44 +0700

    upd logo, add svg file

--- src/lib/components/modules/Header/Logo.svelte
+++ src/lib/components/modules/Header/Logo.svelte
@@ -5,6 +5,6 @@
 </script>
 
 <Link href="/" class="select-none px-2 md:px-4">
-	<Treetale class="h-9 w-auto max-sm:hidden" />
+	<Treetale class="h-8 w-auto max-sm:hidden" />
 	<TreetaleShort class="hidden h-9 w-auto max-sm:block" />
 </Link>
