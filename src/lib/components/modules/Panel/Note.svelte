8f695839a8273869b348b6ad88d730f1b8d0facc
Gleb Winterson <gtwototoo@gmail.com>
2024-01-28 20:15:33 +0700

    bump

--- src/lib/components/modules/Panel/Note.svelte
+++ src/lib/components/modules/Panel/Note.svelte
@@ -8,8 +8,10 @@
 	export let icon: typeof SvelteComponent<unknown>;
 </script>
 
-<div class="flex select-none flex-col items-center gap-4 px-4 pb-4 text-center text-sm">
-	<Icon class="h-12 w-12 text-contrast-9" type={icon} />
+<div class="flex select-none items-center gap-4 px-4 pb-4 text-center text-sm">
+	<div class="rounded-lg bg-contrast-2 p-3">
+		<Icon class="size-6 text-contrast-9" type={icon} />
+	</div>
 	{#if text}
 		{correctWhitespace(text)}
 	{:else}
