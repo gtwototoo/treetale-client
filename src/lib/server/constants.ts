3496eb332d10b78b54e233dded6d15f65281ea5a
Gleb Winterson <gtwototoo@gmail.com>
2024-07-18 18:26:08 +0700

    maybe fix

--- src/lib/server/constants.ts
+++ src/lib/server/constants.ts
@@ -1,19 +1,14 @@
 import type { CookieSerializeOptions } from 'cookie';
 
-import { dev } from '$app/environment';
 import { PUBLIC_TREETALE_CLIENT_URL } from '$env/static/public';
 import ms from 'ms';
 
+const [, domain] = PUBLIC_TREETALE_CLIENT_URL.split('//');
+
 export const COOKIE_OPTIONS: { path: string } & CookieSerializeOptions = {
+	domain,
 	httpOnly: true,
 	maxAge: ms('365d'),
 	path: '/',
-	sameSite: 'lax',
-	secure: !dev
+	sameSite: 'lax'
 };
-
-if (!dev) {
-	const [, domain] = PUBLIC_TREETALE_CLIENT_URL.split('//');
-
-	COOKIE_OPTIONS.domain = `.${domain}`;
-}
